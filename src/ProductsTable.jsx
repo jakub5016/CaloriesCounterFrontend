e6075e31c4ca72bd0e0b5a6d63ca991979165bbd
jakub5016 <me.jakub.gil@gmail.com>
2024-04-09 13:30:06 +0200

    Basic sum-up

--- src/ProductsTable.jsx
+++ src/ProductsTable.jsx
@@ -18,6 +18,7 @@ const mealTypes = [
 ]
 export default function ProductsTable(props) {
   const navigate = useNavigate()
+  
   return (
     <>
     {props.meals.map((column) => (
@@ -26,29 +27,42 @@ export default function ProductsTable(props) {
         <TableHead>
           <TableRow>
             <TableCell sx={{ fontWeight: 'bold' }} >{mealTypes[column.type-1]}</TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold' }}>Ammount</TableCell>
             <TableCell align="right" sx={{ fontWeight: 'bold' }}>Calories</TableCell>
             <TableCell align="right" sx={{ fontWeight: 'bold' }}>Fat&nbsp;(g)</TableCell>
             <TableCell align="right" sx={{ fontWeight: 'bold' }}>Carbs&nbsp;(g)</TableCell>
             <TableCell align="right" sx={{ fontWeight: 'bold' }}>Protein&nbsp;(g)</TableCell>
-            <TableCell align="right" sx={{ fontWeight: 'bold' }}></TableCell>
+            <TableCell align="right" sx={{ fontWeight: 'bold', borderBottom: "none"}}></TableCell>
 
           </TableRow>
         </TableHead>
         <TableBody>
             {
-                column.products.map((row) => (
+                column.products.map((row, index) => (
                 <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                     <TableCell component="th" scope="row">
                     {row.name}
                     </TableCell>
+                    <TableCell align="right">{column.ammoutOfProduct[index]}</TableCell>
                     <TableCell align="right">{row.kcal}</TableCell>
                     <TableCell align="right">{row.fat}</TableCell>
                     <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                    <TableCell align="left"><IconButton style={{border:"1px solid", color:"primary"}}><RemoveIcon/></IconButton></TableCell>
+                    <TableCell align="right" sx={{borderRight: "1px solid"}}>{row.protein}</TableCell>
+                    <IconButton style={{border:"1px solid", color:"primary", marginRight: "0px"}}><RemoveIcon/></IconButton>
                 </TableRow>
                 ))
             }
+
+                <TableRow>
+                <TableCell sx={{ fontWeight: 'bold', color:"green"}}> Calories for this meal:</TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}> </TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}> </TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}> </TableCell>
+                <TableCell sx={{ fontWeight: 'bold' }}> </TableCell>
+
+                  <TableCell align="right"  sx={{ fontWeight: 'bold', color:"green", borderRight: "1px solid" }} >{column.kcalForMeal}</TableCell>
+
+                </TableRow>
         </TableBody>
       </Table>
       <Button variant="outlined" startIcon={<AddIcon/>} onClick={()=>{
